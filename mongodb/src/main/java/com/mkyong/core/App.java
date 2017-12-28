4df72de5356a6203222a2a8777537e059a88791b
Galit Nadler <NadlerGalit@users.noreply.github.com>
2017-12-28 19:30:56 +0200

    Add files via upload

---
+++ mongodb/src/main/java/com/mkyong/core/App.java
@@ -0,0 +1,88 @@
+package com.mkyong.core;
+
+import java.net.UnknownHostException;
+import java.util.Date;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+/**
+ * Java + MongoDB Hello world Example
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+
+		try {
+
+			/**** Connect to MongoDB ****/
+			// Since 2.10.0, uses MongoClient
+			MongoClient mongo = new MongoClient("localhost", 27017);
+
+			/**** Get database ****/
+			// if database doesn't exists, MongoDB will create it for you
+			MongoDatabase db = mongo.getDatabase("testdb");
+
+			/**** Get collection / table from 'testdb' ****/
+			// if collection doesn't exists, MongoDB will create it for you
+			MongoCollection<Document> table = db.getCollection("user");
+
+			/**** Insert ****/
+			// create a document to store key and value
+			Document document = new Document();
+			document.put("name", "mkyong");
+			document.put("age", 30);
+			document.put("createdDate", new Date());
+			table.insertOne(document);
+
+			/**** Find and display ****/
+			BasicDBObject searchQuery = new BasicDBObject();
+			searchQuery.put("name", "mkyong");
+
+			FindIterable<Document> cursor = table.find(searchQuery);
+
+			while (cursor.iterator().hasNext(
+
+			)) {
+				System.out.println(cursor.iterator().next());
+			}
+
+			/**** Update ****/
+			// search document where name="mkyong" and update it with new values
+			BasicDBObject query = new BasicDBObject();
+			query.put("name", "mkyong");
+
+			BasicDBObject newDocument = new BasicDBObject();
+			newDocument.put("name", "mkyong-updated");
+
+			BasicDBObject updateObj = new BasicDBObject();
+			updateObj.put("$set", newDocument);
+
+			table.updateOne(query, updateObj);
+
+			/**** Find and display ****/
+			BasicDBObject searchQuery2 
+				= new BasicDBObject().append("name", "mkyong-updated");
+
+			FindIterable<Document> cursor2 = table.find(searchQuery2);
+
+			while (cursor2.iterator().hasNext()) {
+				System.out.println(cursor2.iterator().next());
+			}
+
+			/**** Done ****/
+			System.out.println("Done");
+
+		} catch (MongoException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
